4f504775915087c40454419afa849312ae5ff0b5
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2022-07-24 00:57:26 +1200

    add cool cosmetic code

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ console.log(" d8'        `8b  88         \"8a,   ,aa 88      88      88 \"8b,   
 console.log("d8'          `8b 88          `\"Ybbd8\"' 88      88      88  `\"Ybbd8\"'   \"Y888 88  `\"Ybbd8\"' `\"8bbdP\"Y8\n\n")
 
 const nodes = [ // optifine is really fickle and won't accept the ip of the nodebalancer, randomly dispersing queries between nodes manually instead
-	//"170.187.157.120",
-	"172.105.135.43"
+	"172.105.135.43", // node 1
+	"172.105.159.99", // node 2
+	"170.187.157.120" // node 3
 ];
 
 var cachedIps = new Object;
@@ -23,32 +24,50 @@ function getNodeIp() {
 	return nodes[Math.floor(Math.random() * nodes.length)];
 }
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.send("Cosmetica has been installed! Nice.");
 });
 
-app.get("/capes/:username", function (req, res) { // optifine
+app.get("/capes/:username", function(req, res) { // optifine
 	var user = req.params.username;
 	if (user.toLowerCase().endsWith(".png")) user = user.substring(0, user.length - 4);
 	var url = "http://" + getNodeIp() + "/get/cloak?user=" + user + "&optifine=show";
 	var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-	res.redirect(301, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
+	res.redirect(302, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
 });
 
-app.get("/MinecraftCloaks/:username", function (req, res) { // old minecraft
+app.get("/MinecraftCloaks/:username", function(req, res) { // old minecraft
 	var user = req.params.username;
 	if (user.toLowerCase().endsWith(".png")) user = user.substring(0, user.length - 4);
 	var url = "http://" + getNodeIp() + "/get/cloak?user=" + user + "&optifine=show";
 	var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-	res.redirect(301, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
+	res.redirect(302, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
 });
 
-app.get("/MinecraftSkins/:username", function (req, res) { // old minecraft
+app.get("/MinecraftSkins/:username", function(req, res) { // old minecraft
 	var user = req.params.username;
 	if (user.toLowerCase().endsWith(".png")) user = user.substring(0, user.length - 4);
 	var url = "http://" + getNodeIp() + "/get/skin?user=" + user + "&optifine=show";
 	var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-	res.redirect(301, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
+	res.redirect(302, url); // i tried this for ages in fastify and optifine wouldn't pick it up. giving up... for now
+});
+
+app.get("/items/:type/:id/:resource", function(req, res) { // cosmetic
+	var type = req.params.type;
+	var id = req.params.id;
+	var resource = req.params.resource;
+	if (!(["model", "texture"]).includes(resource)) return res.status(404).send("invalid resource");
+	var url = "http://" + getNodeIp() + "/get/optifine" + resource + "?type=" + type + "&id=" + id + "&mustbeapproved";
+	res.redirect(302, url);
+});
+
+app.get("/users/:username", function(req, res) {
+	var username = req.params.username;
+	if (username.toLowerCase().endsWith(".cfg")) {
+		username = username.substring(0, username.length - 4);
+	}
+	var url = "http://" + getNodeIp() + "/get/optifineprofile?username=" + username;
+	res.redirect(302, url);
 });
 
 app.listen(80, function() {
